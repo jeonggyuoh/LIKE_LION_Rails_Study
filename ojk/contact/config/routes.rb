87716bb005fe6ff7cf781abad71cf5ab1a194b2d
jeonggyuoh <ohjeonggyu@likelion.org>
2017-02-06 23:44:41 +0900

    controller, view and routes

---
+++ ojk/contact/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'email/email_form' => 'email#email_form'
+  get 'email/email_send' => 'email#email_send'
+end
--- ojk/testApp/app/controllers/home_controller.rb
+++ ojk/testApp/app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+	
+	// 액션당 1개의 뷰 대응!
 	def hello
 		@name = params[:my_name]
 	end
